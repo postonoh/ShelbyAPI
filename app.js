7fa1b5eba6c11abc1449a530257e173ef36cc601
postonoh <postonoh@att.net>
2020-07-29 13:13:06 -0400

    Add file upload for image functions

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 const express = require("express");
 const app = express();
 const Post = require("./api/model/posts");
+var multer = require('multer');
+var upload = multer({dest: 'uploads/'});
 const postData = new Post();
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, `${file.fieldname}-${Date.now()}${getExt(file.mimetype)}`)
+    }
+});
+
+const getExt = (mimetype) => {
+    switch(mimetype){
+        case "image/png":
+            return '.png';
+        case "image/jpeg":
+            return '.jpg';
+    }
+}
+
+var upload = multer({ storage: storage });
 
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -26,5 +47,19 @@ app.get("/api/posts/:post_id", (req, res) => {
     }
 })
 
+app.post("/api/posts",  upload.single('post-image'), (req, res) => {
+    const newPost = {        
+    "id": `${Date.now()}`,
+    "title":  req.body.title,
+    "content": req.body.content,
+    "post_image": req.file.path,
+    "added_date": `${Date.now()}`
+    }
+    res.send(newPost);
+})
 
+app.post('/uploads', upload.single('avatar'), function (req, res, next) {
+  // req.file is the `avatar` file
+  // req.body will hold the text fields, if there were any
+})
 app.listen(3000, () => console.log("Listening on http://localhost:3000"));
